179972f12ba87b25247b8d4f2501ad5f69f1749a
vishalpawar26 <vishalpawar1784@gmail.com>
2024-04-19 15:24:53 +0530

    updated app.js

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://psycho-coders-client.vercel.app",
+    origin: "https://psycho-coders.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   })
