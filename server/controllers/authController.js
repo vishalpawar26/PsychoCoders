f53cf0a36602dc4d4b2e672224cc139640790bb6
vishalpawar26 <vishalpawar1784@gmail.com>
2024-04-30 15:48:38 +0530

    updated authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -84,7 +84,7 @@ exports.login = async (req, res) => {
     res.cookie(String(user._id), token, {
       path: "/",
       expiresIn: new Date(Date.now() + 1000 * 60 * 60 * 24),
-      domain: "psychocoders.vercel.app",
+      domain: ".vercel.app",
       httpOnly: true,
       secure: true,
       sameSite: "None",
